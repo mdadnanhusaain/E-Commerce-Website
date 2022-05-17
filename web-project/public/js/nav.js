6df72c40bf7e1168ee0863556e9701d0529d7f4a
Md Adnan Hussain <mdadnanhusaain@gmail.com>
2022-05-17 20:54:28 +0530

    Updated JS Files

--- web-project/public/js/nav.js
+++ web-project/public/js/nav.js
@@ -5,6 +5,13 @@ const createNav = () => {
         <div class="nav">
             <img src="img/Img/Icons/logo_bordered.png" class="brand-logo" alt="">
             <div class="nav-items">
+                <ul class="links-container">
+                    <li class="link-item"><a href="#" class="link">home</a></li>
+                    <li class="link-item"><a href="#" class="link">women</a></li>
+                    <li class="link-item"><a href="#" class="link">men</a></li>
+                    <li class="link-item"><a href="#" class="link">kids</a></li>
+                    <li class="link-item"><a href="#" class="link">accessories</a></li>
+                </ul>
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
                     <button class="search-btn">search</button>
@@ -19,13 +26,6 @@ const createNav = () => {
                 <a href="#"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
-        <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">kids</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
-        </ul>
     `;
 }
 
